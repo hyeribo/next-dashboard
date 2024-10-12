a4d8961f584db2a364ae0d24d37017bdc679c542
hyeribo <harmfuli59@gmail.com>
2024-10-12 23:56:01 +0900

    debounce 기능 추가

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,14 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
     console.log(`Searching... ${term}`);
 
     // URLSearchParams: 쿼리 스트링 관리 Web API
@@ -19,7 +20,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
