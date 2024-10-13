12353c6e98a3b7e30643a5c6a0564549917e7475
hyeribo <harmfuli59@gmail.com>
2024-10-13 22:31:45 +0900

    Pagination 추가

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -14,6 +14,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
     // URLSearchParams: 쿼리 스트링 관리 Web API
     const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
     if (term) {
       params.set("query", term);
     } else {
