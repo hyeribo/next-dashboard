c54627b552b0575f0ddd5dfee66b27daa3f26aaa
hyeribo <harmfuli59@gmail.com>
2024-11-04 13:29:50 +0900

    누락 코드 추가

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -114,6 +114,9 @@ export async function updateInvoice(
 
 export async function deleteInvoice(id: string) {
   try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices");
+    return { message: "Deleted Invoice" };
   } catch (error) {
     return { message: "Database Error: Failed to Delete Invoice." };
   }
